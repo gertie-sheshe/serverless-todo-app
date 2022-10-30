281d2c1df847b6fbfb7a1f8e704565af78afa36a
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-10-30 14:13:38 +0300

    (feat) port auth + secrets management

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,14 +1,22 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk';
 
-// import { verify, decode } from 'jsonwebtoken'
+
+import { verify } from 'jsonwebtoken'
 
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
-// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
+
+const secretId = process.env.AUTH_0_SECRET_ID
+
+let cachedSecret: string
+
+const client = new AWS.SecretsManager();
+
+
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
@@ -57,13 +65,14 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  console.log(token)
   // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const secret: string = await getSecret();
+
+  return verify(token, secret) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -76,4 +85,21 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getSecret() {
+  if (cachedSecret) return cachedSecret;
+
+  console.log('GET SECRET', secretId)
+
+  const data = await client.getSecretValue({
+    SecretId: secretId
+  }) 
+  .promise();
+
+  console.log('CAHED SECRET', data)
+
+  cachedSecret = data.SecretString;
+
+  return cachedSecret;
 }
